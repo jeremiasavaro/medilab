c4e99162aecf7f25185858de191396983a9ff513
Baachi13 <jeremiasavaro7@gmail.com>
2024-08-26 19:16:29 -0300

    Deleting vite

---
+++ server/app.py
@@ -0,0 +1,11 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {"message": "Hello from Flask!"}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
