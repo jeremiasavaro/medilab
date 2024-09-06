746f418aca4102bf893bd2a9db33d8e2fe55b196
Baachi13 <jeremiasavaro7@gmail.com>
2024-09-06 11:46:54 -0300

    function 'modify_patient' modified, and 'modify_password' implemented

--- backend/db/functions_db.py
+++ backend/db/functions_db.py
@@ -48,15 +48,33 @@ def get_patient(dni):
     conn.close()
     return patient_data
 
-def modify_patient(dni, fieldToChange, value):
+def modify_password(dni, newPassword):
     conn = connect()
     cursor = conn.cursor()
 
-    query = f"UPDATE patient SET {fieldToChange} = ? WHERE dni = ?"
-    cursor.execute(query, (value, dni))
+    query = """UPDATE patient 
+            SET password = ? 
+            WHERE dni = ?"""
+    cursor.execute(query, (newPassword, dni))
 
     conn.commit()
     conn.close()
+
+def modify_patient(dni, firstName, lastName, password, email, phoneNumber, dateBirth, age, nationality, province, locality, postalCode, address, gender):
+    conn = connect()
+    cursor = conn.cursor()
+
+    query = """
+        UPDATE patient 
+        SET firstName = ?, lastName = ?, password = ?, email = ?, phoneNumber = ?, dateBirth = ?, 
+            age = ?, nationality = ?, province = ?, locality = ?, postalCode = ?, address = ?, gender = ?
+        WHERE dni = ?
+    """
+    cursor.execute(query, (firstName, lastName, password, email, phoneNumber, dateBirth, age, nationality, province, locality, postalCode, address, gender, dni))
+
+    conn.commit()
+    conn.close()
+
 
 def get_password(dni):
     conn = connect()
