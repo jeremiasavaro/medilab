af0c294057b9aa2e7896a5229058d8b84c10fd9d
marcelojuarez <marcelo395juarez@gmail.com>
2024-09-17 01:10:51 -0300

    some advances in the x-ray section

--- frontend/src/components/XrayService.jsx
+++ frontend/src/components/XrayService.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from 'react';
+import '../assets/css/XrayService.css';
 
-const XrayService = () =>{
+const XrayService = ({setView}) =>{
     const [message, setMessage] = useState('');
+    const [openSection, setOpenSection] = useState('');
     const handleXrayService = async (e) =>{
     try {
         const response = await fetch('http://127.0.0.1:5000/XrayService', {
@@ -20,14 +22,73 @@ const XrayService = () =>{
           setMessage('Error en la conexión');
         }
     };
+
+    // Con esta funcion manejamos las secciones sobre la side-bar
+    const openOverlaySection = (sectionName) => {
+      setOpenSection(sectionName);
+    };  
+
+    //con esta funcion cerramos una seccion abierta
+    const closeOverlaySection = () => {
+      setOpenSection('');
+    ;}
+
     return (
-        <div className = "gen" > 
-            <h2>Request X-ray Service</h2>
-            <br></br>
-            <p>
-              --
-            </p>
-        </div>
+        <section id="xray-section" className='contentXray'>
+          <header className="title">Faster Diagnostics with X-RAI</header>
+            <div className="side-Bar">
+                <button className="toggle-button" onClick={() => openOverlaySection('Method')}>
+                  Method Used
+               </button>
+               <button className="toggle-button" onClick={() => openOverlaySection('Diagnostics')}>
+                  Diagnostics options
+               </button>
+               <button className="toggle-button" onClick={() => openOverlaySection('Fiability')}>
+                  Fiability
+               </button>
+                <ul>
+                    <li onClick={() => setView('home')}><i className=""></i>
+                      <span className="back">
+                      Back To Main Page
+                      </span>
+                       
+                    </li>
+                </ul>
+            </div>
+            {openSection === 'Method' && (
+             <div className="overlay-section">
+              <div className="overlay-content">
+               <h2>Method</h2>
+                <p>Aca vamos a contar sobre el metodo usado</p>
+                <button onClick={closeOverlaySection}>Close</button>
+              </div>
+            </div>
+            )}
+            {openSection === 'Diagnostics' && (
+             <div className="overlay-section">
+              <div className="overlay-content">
+               <h2>Method</h2>
+                <p>Aca deberiamos colocar los distintos diagnosticos que se podria hacer</p>
+                <button onClick={closeOverlaySection}>Close</button>
+              </div>
+            </div>
+            )}
+            {openSection === 'Fiability' && (
+             <div className="overlay-section">
+              <div className="overlay-content">
+               <h2>Method</h2>
+                <p>Aca podriamos argumentar sobre la fiabilidad del metodo</p>
+                <button onClick={closeOverlaySection}>Close</button>
+              </div>
+            </div>
+            )}
+           {/* 
+            <h3 className="message">Your result in one click</h3>
+            <button className="Initiate-button">
+                  Initiate
+            </button>
+            */}
+        </section>
     );
 }   
 export default XrayService;
