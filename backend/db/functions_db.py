4fd619976aae03c719224efc4a245bc4f20245a2
MatiasPellizzari <matiaspellizzari0@gmail.com>
2024-10-04 21:08:03 -0300

    Most functions modified for sqlAlchemy, only three left. TEST NEEDED

--- backend/db/functions_db.py
+++ backend/db/functions_db.py
@@ -1,6 +1,8 @@
 import sqlite3
 from datetime import datetime
 from flask import current_app
+from db.database import db
+from db.models import patient, doctor, clinic, diagnostic, worksAt
 
 def connect(testing=False):
     try:
@@ -16,96 +18,84 @@ def connect(testing=False):
         print(f"Error connecting to database: {e}")
         return None
 
-def insert_patient(dni, firstName, lastName, password, email, phoneNumber, dateBirth, nationality, province,
-                   locality, postalCode, address, gender, imagePatient=None):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("""INSERT INTO patient (dni, firstName, lastName, password, email, phoneNumber, dateBirth, age, nationality, province, 
-                   locality, postalCode, address, gender, imagePatient) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)""",
-                   (dni, firstName, lastName, password, email, phoneNumber, dateBirth, calculate_age(dateBirth),
-                    nationality, province,
-                    locality, postalCode, address, gender, imagePatient))
-
-    conn.commit()
-    conn.close()
+def insert_patient(dni, first_name, last_name, encrypted_password, email, phone_number, date_birth,nationality, province, locality, postal_code, address, gender):
+    new_patient = patient(
+        dni=dni,
+        first_name=first_name,
+        last_name=last_name,
+        password=encrypted_password,
+        email=email,
+        phone_numbere=phone_number,
+        date_birth=date_birth,
+        nationality=nationality,
+        province=province,
+        locality=locality,
+        postal_code=postal_code,
+        address=address,
+        gender=gender
+    )
+    db.session.add(new_patient)
+    db.session.commit()
 
 
 def delete_patient(dni):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM patient WHERE dni = ?", (dni,))
-    print(f"patient with DNI {dni} successfully removed")
+    patient = patient.query.filter_by(dni=dni).first()
     
-    conn.commit()
-    conn.close()
+    if patient:
+        db.session.delete(patient)
+        db.session.commit()
+        print(f"Patient with DNI {dni} successfully removed")
+    else:
+        print(f"Patient with DNI {dni} not found")
 
 #Faltaria arreglar que si se elimina un paciente se deberian eliminar sus diagnosticos correspondientes
 #Ya esta hecho con la foreign key!!
 
 
 def get_patient(dni):
-    testing = current_app.config['TESTING'] if current_app else False  
-    conn = connect(testing=testing)
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM patient WHERE dni = ?", (dni,))
-    patient_data = cursor.fetchone()
-
-    conn.close()
-    return patient_data
+    #testing = current_app.config.get('TESTING', False)
+    
+    patient = patient.query.filter_by(dni=dni).first()
+    return patient
 
 
 def modify_password(dni, newPassword):
-    conn = connect()
-    cursor = conn.cursor()
-
-    query = """UPDATE patient 
-            SET password = ? 
-            WHERE dni = ?"""
-    cursor.execute(query, (newPassword, dni))
-
-    conn.commit()
-    conn.close()
+    patient = patient.query.filter_by(dni=dni).first()
+    if patient:
+        patient.password = newPassword
+        db.session.commit()
 
 def modify_image_patient(dni, imagePatient):
-    conn = connect()
-    cursor = conn.cursor()
-
-    query = """UPDATE patient
-            SET imagePatient = ?
-            WHERE dni = ?"""
-    cursor.execute(query, (imagePatient, dni))
-
-    conn.commit()
-    conn.close()
+    patient = patient.query.filter_by(dni=dni).first()
+    if patient:
+        patient.image_patient = imagePatient
+        db.session.commit()
 
 def modify_patient(dni, firstName, lastName, email, phoneNumber, dateBirth, nationality, province, locality, postalCode, address, gender, imagePatient=None):
-    conn = connect()
-    cursor = conn.cursor()
-    query = """
-        UPDATE patient 
-        SET firstName = ?, lastName = ?, email = ?, phoneNumber = ?, dateBirth = ?, 
-            age = ?, nationality = ?, province = ?, locality = ?, postalCode = ?, address = ?, gender = ?, imagePatient = ?
-        WHERE dni = ?
-    """
-    cursor.execute(query, (firstName, lastName, email, phoneNumber, dateBirth, calculate_age(dateBirth), nationality, province, locality, postalCode, address, gender, imagePatient, dni))
-
-    conn.commit()
-    conn.close()
-
-
+    patient = patient.query.filter_by(dni=dni).first()
+    
+    if patient:
+        patient.first_name = firstName
+        patient.last_name = lastName
+        patient.email = email
+        patient.phone_number = phoneNumber
+        patient.date_birth = dateBirth
+        patient.age = calculate_age(dateBirth)
+        patient.nationality = nationality
+        patient.province = province
+        patient.locality = locality
+        patient.postal_code = postalCode
+        patient.address = address
+        patient.gender = gender
+        patient.image_patient = imagePatient
+        
+        db.session.commit()
 
 def get_password(dni):
-    conn = connect(testing=app.config['TESTING'])
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT password FROM patient WHERE dni = (?)", (dni,))
-    password_data = cursor.fetchone()
-
-    conn.close()
-    return password_data[0]
+    patient = patient.query.filter_by(dni=dni).first()
+    if patient:
+        return patient.password
+    return None  # Return None if no patient is found with the given DNI
 
 
 def calculate_age(dateBirth):
@@ -116,124 +106,94 @@ def calculate_age(dateBirth):
 
 
 def insert_diagnostic(result, description, imageDiagnostic, dni):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("""
-        INSERT INTO diagnostic (result, description, imageDiagnostic, dni) 
-        VALUES (?,?,?,?)
-    """, (result, description, imageDiagnostic, dni))
-
-    # Obtengo el último ID
-    cod = cursor.lastrowid
-
-    conn.commit()
-    conn.close()
-
-    return cod
+     new_diagnostic = diagnostic(
+        res=result,
+        description=description,
+        image_diagnostic=imageDiagnostic,
+        dni=dni,
+     )
+     db.session.add(new_diagnostic)
+     db.session.commit()
 
 
 def get_diagnostics(dni):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM diagnostic WHERE dni = ?", (dni,))
-    patient_data = cursor.fetchall()
-
-    conn.close()
-    return patient_data
+    #testing = current_app.config.get('TESTING', False)
+    
+    diagnostics = diagnostic.query.filter_by(dni=dni).all()
+    return diagnostics
 
 def get_diagnostics_by_code(cod):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM diagnostic WHERE cod = ?", (cod,))
-    patient_data = cursor.fetchall()
-
-    conn.close()
-    return patient_data
+    #testing = current_app.config.get('TESTING', False)
+    
+    diagnostics = diagnostic.query.filter_by(cod=cod).all()
+    return diagnostics
 
 def get_diagnostics_by_result(dni, result):
-    conn = connect()
-    cursor = conn.cursor()
+    #testing = current_app.config.get('TESTING', False)
 
-    cursor.execute("SELECT * FROM diagnostic WHERE (dni,result) = (?,?)", (dni, result))
-    patient_data = cursor.fetchall()
-
-    conn.close()
-    return patient_data
+    diagnostics = diagnostic.query.filter_by(dni=dni, result=result).all()
+    return diagnostics
 
 def get_diagnostics_by_description(dni, description):
-    conn = connect()
-    cursor = conn.cursor()
+     #testing = current_app.config.get('TESTING', False)
 
-    cursor.execute("SELECT * FROM diagnostic WHERE (dni,description) = (?,?) ", (dni, description))
-    patient_data = cursor.fetchall()
-    
-    conn.close()
-    return patient_data
+    diagnostics = diagnostic.query.filter_by(dni=dni, description=description).all()
+    return diagnostics
 #-------------------------------------------- Doctor Table functions --------------------------------------------
-def insert_doctor(dni,matricule, firstName, lastName, email, phoneNumber, dateBirth, gender, imageDoctor=None):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("""INSERT INTO doctor (dni, speciality, firstName, lastName, email, phoneNumber, dateBirth, age, gender, imageDoctor) 
-                  VALUES (?,?,?,?,?,?,?,?,?,?)""", 
-               (dni, matricule, firstName, lastName, email, phoneNumber, dateBirth, calculate_age(dateBirth), gender, imageDoctor))
-
-    conn.commit()
-    conn.close()
+def insert_doctor(dni,speciality, firstName, lastName, email, phoneNumber, dateBirth, gender, imageDoctor=None):
+     new_doctor = doctor(
+        dni=dni,
+        speciality=speciality,
+        first_name=firstName,
+        last_name=lastName,
+        email=email,
+        phone_number=phoneNumber,
+        date_birth=dateBirth,
+        age=calculate_age(dateBirth),
+        gender=gender,
+        image_doctor=imageDoctor,
+     )
+     db.session.add(new_doctor)
+     db.session.commit()
 
 
 def delete_doctor(dni):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM doctor WHERE dni = ?", (dni,))
-    print(f"doctor with DNI {dni} successfully removed")
+    doctor = doctor.query.filter_by(dni=dni).first()
     
-    conn.commit()
-    conn.close()
+    if patient:
+        db.session.delete(doctor)
+        db.session.commit()
+        print(f"Doctor with DNI {dni} successfully removed")
+    else:
+        print(f"Doctor with DNI {dni} not found")
 
-def modify_doctor(dni,matricule, firstName, lastName, email, phoneNumber, dateBirth, gender, imageDoctor=None):
-    conn = connect()
-    cursor = conn.cursor()
-
-    query = """
-        UPDATE doctor 
-        SET matricule = ?, firstName = ?, lastName = ?, email = ?, phoneNumber = ?, dateBirth = ?, 
-            age = ?, gender = ?, imageDoctor = ?
-        WHERE dni = ?
-    """
-    cursor.execute(query, (matricule,firstName, lastName, email, phoneNumber, dateBirth, calculate_age(dateBirth),gender, imageDoctor, dni))
-
-    conn.commit()
-    conn.close()
+def modify_doctor(dni,speciality, firstName, lastName, email, phoneNumber, dateBirth, gender, imageDoctor=None):
+    doctor = doctor.query.filter_by(dni=dni).first()
+    
+    if doctor:
+        doctor.speciality = speciality
+        doctor.first_name = firstName
+        doctor.last_name = lastName
+        doctor.email = email
+        doctor.phone_number = phoneNumber
+        doctor.date_birth = dateBirth
+        doctor.age = calculate_age(dateBirth)
+        doctor.gender = gender
+        doctor.image_doctor = imageDoctor
+        
+        db.session.commit()
 
 def get_doctors():
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM doctor")
-    doctor_data = cursor.fetchall()
-
-    # Print the result to check if the query returns any data
-    print(f"Doctors fetched: {doctor_data}")
-
-    conn.close()
-
-    return doctor_data
+    #testing = current_app.config.get('TESTING', False)
+    
+    doctors = doctor.query.all()
+    return doctors
 
 def get_doctors_by_speciality(doctor_speciality):
-    conn = connect()
-    cursor = conn.cursor()
-
-    # SQL query to get doctors by matricule
-    cursor.execute("SELECT firstname, speciality FROM doctor WHERE speciality = ?", (doctor_speciality,))
-
-    doctor_data = cursor.fetchall()
-
-    print(f"Doctors fetched: {doctor_data}")
+    #testing = current_app.config.get('TESTING', False)
+    
+    doctors = doctor.query.filter_by(speciality=doctor_speciality).all()
+    return doctors
 
     conn.close()
 
@@ -272,82 +232,52 @@ def get_cities_for_doctor(doctor_dni):
 
 #-------------------------------------------- Clinic Table functions --------------------------------------------
 def insert_clinic(name, phoneNumber, province, city, postalCode, address):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("""INSERT INTO clinic (Name, phoneNumber, province, city, postalCode, address) 
-                      VALUES (?, ?, ?, ?, ?, ?)""",
-                   (name, phoneNumber, province, city, postalCode, address))
-
-    conn.commit()
-    conn.close()
+    new_clinic = clinic(
+        name=name,
+        phone_number=phoneNumber,
+        province=province,
+        city=city,
+        postal_code=postalCode,
+        address=address,
+     )
+    db.session.add(new_clinic)
+    db.session.commit()
 
 def delete_clinic(name):
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("DELETE FROM clinic WHERE name = ?", (name,))
-    print(f"clinic with name {name} successfully removed")
+    clinic = clinic.query.filter_by(name=name).first()
     
-    conn.commit()
-    conn.close()    
+    if patient:
+        db.session.delete(clinic)
+        db.session.commit()
+        print(f"Clinic with name {name} successfully removed")
+    else:
+        print(f"Clinic with name {name} not found")   
 
 def modify_clinic(name, phoneNumber, province, city, postalCode, address):
-    conn = connect()
-    cursor = conn.cursor()
+    clinic = clinic.query.filter_by(name=name).first()
+    
+    if clinic:
+        clinic.name = name
+        clinic.phone_number = phoneNumber
+        clinic.province = province
+        clinic.city = city
+        clinic.postal_code = postalCode
+        clinic.address = address
 
-    #Find the clinic number based on the  name
-    find_query = "SELECT clinic_number FROM clinic WHERE name = ?"
-    cursor.execute(find_query, (name,))
-    clinic_number = cursor.fetchone()
-
-    if clinic_number is None:
-        print("Clinic not found.")
-        return
-
-    #Update the clinic details using the clinic_number
-    query = """
-        UPDATE clinic 
-        SET phoneNumber = ?, province = ?, city = ?, postalCode = ?, address = ? 
-        WHERE clinic_number = ?
-    """
-    cursor.execute(query, (phoneNumber, province, city, postalCode, address, clinic_number[0]))
-
-    conn.commit()
-    conn.close()
-
-    print(f"Clinic '{name}' has been updated successfully.")
+        db.session.commit()
 
 def get_all_clinics():
-    conn = connect()
-    cursor = conn.cursor()
-
-    cursor.execute("SELECT * FROM clinic")
-    clinic_data = cursor.fetchall()
-
-    # Print the result to check if the query returns any data
-    print(f"Clinics fetched: {clinic_data}")
-
-    conn.close()
-
-    return clinic_data    
+   #testing = current_app.config.get('TESTING', False)
+    
+    doctors = doctor.query.all()
+    return doctors   
 
 def get_clinics_by_city(cityname):
-    conn = connect()
-    cursor = conn.cursor()
-
-   # Find the clinic number based on the name
-    find_query = "SELECT * FROM clinic WHERE city = ?"
-    cursor.execute(find_query, (cityname,))
-
-    clinic_data = cursor.fetchall()
-
-    # Print the result to check if the query returns any data
-    print(f"Clinics fetched: {clinic_data}")
-
-    conn.close()
-
-    return clinic_data  
+   #testing = current_app.config.get('TESTING', False)
+    
+    doctors = doctor.query.filter_by(name=cityname).all()
+    return doctors
+ 
 
 
 #-------------------------------------------- WorksAt Table functions --------------------------------------------
