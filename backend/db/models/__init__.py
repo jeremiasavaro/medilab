5e158aad220bf7f7a3d00e87a0f819b4ca3e7d16
MateoV8 <ccornejomateo@gmail.com>
2024-10-20 18:17:37 -0300

    changed paths in database files because they were not recognised

---
+++ backend/db/models/__init__.py
@@ -0,0 +1,9 @@
+# backend/db/models/__init__.py
+
+# Importar modelos para que se puedan acceder desde el paquete
+
+from .patient import Patient
+from .doctor import Doctor
+from .clinic import Clinic
+from .diagnostic import Diagnostic
+from .worksAt import WorksAt
