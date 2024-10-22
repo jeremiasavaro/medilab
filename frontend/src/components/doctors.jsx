9e42287310ee1c94dd8669bb89212ecb9efd4ebf
MateoV8 <ccornejomateo@gmail.com>
2024-10-22 10:25:50 -0300

    show all doctors (loaded in the seed) in the doctors section

--- frontend/src/components/doctors.jsx
+++ frontend/src/components/doctors.jsx
@@ -1,9 +1,34 @@
 // src/components/Doctors.jsx
 
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import doctorsBg from "../assets/img/doctors/doctors-1.jpg";
 
 function Doctors() {
+
+  const [doctors, setDoctors] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDoctors = async () => {
+      try {
+        const response = await fetch('http://localhost:5000/doctors');  // Cambia a la URL correcta de tu API
+        const data = await response.json();
+        console.log('Doctors data:', data); // Verificar los datos aquí
+        setDoctors(data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching doctors:', error);
+        setLoading(false);
+      }
+    };
+  
+    fetchDoctors();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
   return (
     <section id="doctors" className="doctors section">
       <div className="container section-title" data-aos="fade-up">
@@ -13,22 +38,17 @@ function Doctors() {
 
       <div className="container">
         <div className="row gy-4">
-          <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100">
-            <div className="team-member d-flex align-items-start">
-              <div className="pic"><img src={doctorsBg} className="img-fluid" alt="" /></div>
-              <div className="member-info">
-                <h4>Walter White</h4>
-                <span>Pulmonologist</span>
-                <p>Un neumonólogo :)</p>
-                <div className="social">
-                  <a href="#"><i className="bi bi-twitter-x"></i></a>
-                  <a href="#"><i className="bi bi-facebook"></i></a>
-                  <a href="#"><i className="bi bi-instagram"></i></a>
-                  <a href="#"><i className="bi bi-linkedin"></i></a>
+          {doctors.map((doctor, index) => (
+            <div className="col-lg-6" data-aos="fade-up" data-aos-delay="100" key={index}>
+              <div className="team-member d-flex align-items-start">
+                <div className="member-info">
+                  <h4>{doctor.first_name} {doctor.last_name}</h4>
+                  <span>{doctor.speciality}</span>
+                  {/* Agrega más información del doctor si es necesario */}
                 </div>
               </div>
             </div>
-          </div>
+          ))}
         </div>
       </div>
     </section>
