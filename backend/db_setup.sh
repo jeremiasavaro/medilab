94802bb290ca7fe2011b0a735d56df34eedfe33a
maximarquezregis <maximarquez2004@gmail.com>
2024-10-27 14:44:17 -0300

    db_setup script modified

--- backend/db_setup.sh
+++ backend/db_setup.sh
@@ -2,14 +2,14 @@
 
 # Initialize the database with flask 
 echo "Initializing database..."
-flask db init
+pipenv run flask db init
 
 # Generate the database migration
 echo "Generating migrate..."
-flask db migrate
+pipenv run flask db migrate
 
 # Apply migrations
 echo "Applying all migrations..."
-flask db upgrade
+pipenv run flask db upgrade
 
 echo "Database configuration process completed."
