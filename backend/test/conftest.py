ad1b3bd0bad5543932eab154aa22733bbe8e4ce4
marcelojuarez <marcelo395juarez@gmail.com>
2024-11-03 18:02:38 -0300

    test for blueprint inquiries implemented

--- backend/test/conftest.py
+++ backend/test/conftest.py
@@ -1,18 +1,41 @@
 import sys
 import os
 import pytest
+from datetime import date
 
 # Add the backend directory to the Python path
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
-from factory.__init__ import create_app
+from factory.__init__ import *
 from config.config import TestingConfig  # Import the TestingConfig class
 
 @pytest.fixture
 def app():
     app = create_app(TestingConfig)
+    
     with app.app_context():
-        yield app
+        
+        db.create_all()
+    
+        test_doctor = Doctor(
+            dni=12345678,  
+            speciality="Cardiology",  
+            first_name="Jane",  
+            last_name="Doe",  
+            email="jane.doe@example.com",  
+            phone_number=1234567890,  
+            date_birth=date(1985, 5, 15),  
+            age=39, 
+            gender="Female"  
+        )        
+        
+        db.session.add(test_doctor)
+        db.session.commit()
+
+    yield app
+
+    with app.app_context():
+        db.drop_all()
 
 @pytest.fixture
 def client(app):
