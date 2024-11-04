817b52361577ece0b66719e0992f66e8fefe905c
marcelojuarez <marcelo395juarez@gmail.com>
2024-11-04 19:19:48 -0300

    some more tests on auth blueprint

--- backend/test/test_auth.py
+++ backend/test/test_auth.py
@@ -2,21 +2,27 @@
 from conftest import *
 
 data = {
-        'firstName': 'John',
-        'lastName': 'Doe',
-        'password': 'secure_password',
-        'repPassword': 'secure_password',
-        'address': '123 Main St',
-        'email': 'newuser@example.com',
-        'dni': '12345678',
-        'phone': '1234567890',
-        'birthDate': '1980-01-01',
-        'nationality': 'American',
-        'province': 'California',
-        'locality': 'Los Angeles',
-        'postalCode': '90001',
-        'gender': 'Male'
-    }
+    'firstName': 'John',
+    'lastName': 'Doe',
+    'password': 'secure_password',
+    'repPassword': 'secure_password',
+    'address': '123 Main St',
+    'email': 'newuser@example.com',
+    'dni': '12345678',
+    'phone': '1234567890',
+    'birthDate': '1980-01-01',
+    'nationality': 'American',
+    'province': 'California',
+    'locality': 'Los Angeles',
+    'postalCode': '90001',
+    'gender': 'Male'
+}
+
+def test_obtaint_token(client):
+    response = client.get('/auth/obtainToken')
+
+    assert response.status_code == 200
+    assert 'token' in response.json
 
 def test_user_registration(client):
     response = client.post('/auth/register', json=data)
@@ -45,8 +51,37 @@ def test_user_registration_missing_data(client):
     assert response.status_code == 400
     assert response.json == {'error': 'Data missing'}
 
+def test_user_registration_password_dont_match(client):
+    invalid_data = {
+        'firstName': 'John',
+        'lastName': 'Doe',
+        'password': 'secure_password',
+        'repPassword': 'wrong_password',
+        'address': '123 Main St',
+        'email': 'newuser@example.com',
+        'dni': '12345678',
+        'phone': '1234567890',
+        'birthDate': '1980-01-01',
+        'nationality': 'American',
+        'province': 'California',
+        'locality': 'Los Angeles',
+        'postalCode': '90001',
+        'gender': 'Male'
+    }
+    response = client.post('/auth/register', json=invalid_data)
+
+    assert response.status_code == 400
+    assert response.json == {'error': 'Passwords don\'t match'}
+
+def test_user_registration_user_already_exists(client):
+    response = client.post('/auth/register', json=data)
+    response = client.post('/auth/register', json=data)
+
+    assert response.status_code == 409
+    assert response.json == {'error': 'User already exists'}
+
+
 def test_user_login(client):
-    # Register the user first
     client.post('/auth/register', json=data)
 
     # Now attempt to login
@@ -59,12 +94,36 @@ def test_user_login(client):
     assert response.status_code == 200
     assert 'token' in response.json
 
-def test_user_login_invalid_credentials(client):
+
+def test_user_login_missing_credentials(client):
     invalid_data = {
         'dni': '12345678',
-        'password': 'wrong_password'
     }
     response = client.post('/auth/login', json=invalid_data)
 
+    assert response.status_code == 400
+    assert response.json == {'error': 'Data missing'}
+
+def test_user_login_invalid_dni(client):
+    client.post('/auth/register', json=data)
+    valid_data = {
+        'dni': '12345677',
+        'password': 'secure_password'
+    }
+    response = client.post('/auth/login', json=valid_data)
+
     assert response.status_code == 401
     assert response.json == {'error': 'No user registered with that DNI'}
+
+def test_user_login_incorrect_credentials(client):
+    client.post('/auth/register', json=data)
+
+    # Now attempt to login
+    login_data = {
+        'dni': '12345678',
+        'password': 'wrong_password'
+    }
+    response = client.post('/auth/login', json=login_data)
+
+    assert response.status_code == 401
+    assert response.json == {'error': 'Incorrect credentials'}
