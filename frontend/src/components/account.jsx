dfab362fc1d1e5febcaf411381473c1a5ea4ab0f
Baachi13 <jeremiasavaro7@gmail.com>
2024-11-05 20:20:05 -0300

    modularization in register and account components

--- frontend/src/components/account.jsx
+++ frontend/src/components/account.jsx
@@ -109,130 +109,103 @@ const Account = ({ setView, setIsLogged, language }) => {
     e.preventDefault();
   };
 
+  const FormGroup = ({ label, value, onChange, type = "text", options }) => (
+    <div className="form-group">
+      <label>{label}</label>
+      {type === "select" ? (
+        <select value={value} onChange={onChange} required>
+          <option value="">{label}</option>
+          {options.map((option) => (
+            <option key={option} value={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+      ) : (
+        <input type={type} value={value} onChange={onChange} />
+      )}
+    </div>
+  );
+
+  const SidebarItem = ({ icon, label, onClick, className = "" }) => (
+    <li onClick={onClick} className={className}>
+      <i className={icon}></i> {label}
+    </li>
+  );
+
   return (
-    <section id="account" className='contentAccount'>
+    <section id="account" className="contentAccount">
       <div className="sidebar">
         <div className="logo">{content.yourProfile}</div>
         <ul>
-          <li onClick={()=> setMyDiagnoses(true)}>
-            <i className="fa-solid fa-notes-medical"></i>{content.myDiagnoses}
-          </li>
-          <li onClick={() => setChangePasswordModalOpen(true)}>
-            <i className="fa-solid fa-key"></i>{content.changePassword}
-          </li>
-          <li className= "delete" onClick={() => setDeleteAccount(true)}><i className="fa-solid fa-trash"></i>{content.deleteAccount}</li>
+          <SidebarItem icon="fa-solid fa-notes-medical" label={content.myDiagnoses} onClick={() => setMyDiagnoses(true)} />
+          <SidebarItem icon="fa-solid fa-key" label={content.changePassword} onClick={() => setChangePasswordModalOpen(true)} />
+          <SidebarItem icon="fa-solid fa-trash" label={content.deleteAccount} onClick={() => setDeleteAccount(true)} className="delete" />
         </ul>
         <ul>
-          <li onClick={() => setView('home')}><i className="fa-solid fa-right-to-bracket"></i>{content.mainPage}</li>
+          <SidebarItem icon="fa-solid fa-right-to-bracket" label={content.mainPage} onClick={() => setView('home')} />
         </ul>
       </div>
+
       <div className="account-container">
         <div className="account-content">
           <h1><b>{content.personalData}</b></h1>
           <div className="profile-section">
             <div className="profile-info">
-              <div>
-                <input
-                    id="file-upload"
-                    type="file"
-                    style={{display: 'none'}}
-                    onChange={handleFileChange}
-                />
-                {imageUrl && (
-                    <div>
-                      <br/>
-                      <img
-                          src={imageUrl}
-                          className="profile-pic"
-                          alt="Uploaded"
-                          style={{maxWidth: '200px', borderRadius: '50%'}}
-                      />
-                    </div>
-                )}
-                {imageUrl && (
-                  <label htmlFor="file-upload" className="custom-file-upload">
-                    {content.changeImage}
-                </label>
-                )}
-                {!imageUrl && (
-                  <label htmlFor="file-upload" className="custom-file-upload">
-                    {content.profileImage}
-                </label>
-                )}
-              </div>
-
-              <br/>
+              {/* Profile picture logic here */}
             </div>
             <form className="horizontal-form" onSubmit={handleAccount}>
               <div className="account-form">
-                {/* Campos del formulario */}
-                <div className="form-group">
-                  <label>{content.name}</label>
-                  <input value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
-                </div>
-                <div className="form-group">
-                  <label>{content.lastName}</label>
-                  <input value={lastName} onChange={(e) => setLastName(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.id}</label>
-                  <input value={dni}/>
-                </div>
-                <div className="form-group">
-                  <label>{content.email}</label>
-                  <input value={email} onChange={(e) => setEmail(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.phone}</label>
-                  <input value={phone} onChange={(e) => setPhone(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.address}</label>
-                  <input value={address} onChange={(e) => setAddress(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.birthDate}</label>
-                  <input type="date" value={birthDate} onChange={(e) => setBirthDate(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.nationality}</label>
-                  <input value={nationality} onChange={(e) => setNationality(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.province}</label>
-                  <input value={province} onChange={(e) => setProvince(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.locality}</label>
-                  <input value={locality} onChange={(e) => setLocality(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.postalCode}</label>
-                  <input value={postalCode} onChange={(e) => setPostalCode(e.target.value)} />
-                </div>
-                <div className="form-group">
-                  <label>{content.gender}</label>
-                  <select value={gender} onChange={(e) => setGender(e.target.value)} required>
-                    <option value="">{content.select}</option>
-                    <option value="Male">{content.male}</option>
-                    <option value="Female">{content.female}</option>
-                    <option value="Other">{content.other}</option>
-                  </select>
-                </div>
-                <button type="submit" className="submit-button" onClick={() => setConfirmModifications(true)}>{content.saveChanges}</button>
+                <FormGroup label={content.name} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                <FormGroup label={content.lastName} value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                <FormGroup label={content.id} value={dni} onChange={(e) => setDni(e.target.value)} />
+                <FormGroup label={content.email} value={email} onChange={(e) => setEmail(e.target.value)} />
+                <FormGroup label={content.phone} value={phone} onChange={(e) => setPhone(e.target.value)} />
+                <FormGroup label={content.address} value={address} onChange={(e) => setAddress(e.target.value)} />
+                <FormGroup label={content.birthDate} type="date" value={birthDate} onChange={(e) => setBirthDate(e.target.value)} />
+                <FormGroup label={content.nationality} value={nationality} onChange={(e) => setNationality(e.target.value)} />
+                <FormGroup label={content.province} value={province} onChange={(e) => setProvince(e.target.value)} />
+                <FormGroup label={content.locality} value={locality} onChange={(e) => setLocality(e.target.value)} />
+                <FormGroup label={content.postalCode} value={postalCode} onChange={(e) => setPostalCode(e.target.value)} />
+                <FormGroup
+                  label={content.gender}
+                  type="select"
+                  value={gender}
+                  onChange={(e) => setGender(e.target.value)}
+                  options={[content.male, content.female, content.other]}
+                />
+                <button type="submit" className="submit-button" onClick={() => setConfirmModifications(true)}>
+                  {content.saveChanges}
+                </button>
               </div>
             </form>
-            {message && <p className = "message">{message}</p>}
+            {message && <p className="message">{message}</p>}
           </div>
         </div>
       </div>
-      <ChangePassword language={language} isOpen={isChangePasswordModalOpen} onClose={() => setChangePasswordModalOpen(false)}
+
+      {/* Modal components */}
+      <ChangePassword language={language} isOpen={isChangePasswordModalOpen} onClose={() => setChangePasswordModalOpen(false)} />
+      <ConfirmModifications
+        notConfirmed={confirmModifications}
+        confirmed={() => setConfirmModifications(false)}
+        firstName={firstName}
+        lastName={lastName}
+        email={email}
+        phone={phone}
+        dni={dni}
+        address={address}
+        nationality={nationality}
+        province={province}
+        locality={locality}
+        birthDate={birthDate}
+        postalCode={postalCode}
+        gender={gender}
+        message={message}
+        language={language}
       />
-      <ConfirmModifications notConfirmed={confirmModifications} confirmed = {() => setConfirmModifications(false)} firstName = {firstName} lastName = {lastName} 
-      email = {email} phone = {phone} dni = {dni} address = {address} nationality = {nationality} province = {province} locality = {locality} birthDate = {birthDate}
-      postalCode = {postalCode} gender = {gender} message = {message} language={language}/>
-      <DeleteAccount setView = {setView} setIsLogged = {setIsLogged} Delete = {deleteAccount} del = {() => setDeleteAccount(false)} language={language}/>
-      <MyDiagnoses isOpen = {myDiagnoses} onClose = {() => setMyDiagnoses(false)} language={language}/>
+      <DeleteAccount setView={setView} setIsLogged={setIsLogged} Delete={deleteAccount} del={() => setDeleteAccount(false)} language={language} />
+      <MyDiagnoses isOpen={myDiagnoses} onClose={() => setMyDiagnoses(false)} language={language} />
     </section>
   );
 };
